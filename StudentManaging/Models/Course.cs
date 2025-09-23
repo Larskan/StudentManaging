01f938f240cfcaa96a2f9ff143df9ca0f716ae4a
Lars <Bruger@DESKTOP-DUN3DKK>
2025-09-24 00:38:46 +0200

    Modifies Course Model with new attribute type

--- StudentManaging/Models/Course.cs
+++ StudentManaging/Models/Course.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StudentManaging.Models
 {
     public class Course
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        public int? Credits { get; set; }
+        // Sqlite does not support decimal, so we specify precision and scale for other DBs
+        [Column(TypeName = "decimal(5, 2)")]
+        public decimal? Credits { get; set; }
         public int InstructorId { get; set; } // Foreign key to Instructor
         public Instructor? Instructor { get; set; } // Navigation property to Instructor
 
