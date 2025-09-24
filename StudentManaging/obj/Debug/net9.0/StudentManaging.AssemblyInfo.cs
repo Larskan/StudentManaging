ca70a99d72745f9a69206a5722d3977827cc09fa
Lars <Bruger@DESKTOP-DUN3DKK>
2025-09-24 02:20:56 +0200

    Adds V2 Full and Partial state approach

--- StudentManaging/obj/Debug/net9.0/StudentManaging.AssemblyInfo.cs
+++ StudentManaging/obj/Debug/net9.0/StudentManaging.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManaging")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bab8463ff0a53f6a2537940116b4e24dc50955c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72e253580b371b0e5d70ddada9a9b2353b605205")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManaging")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManaging")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
